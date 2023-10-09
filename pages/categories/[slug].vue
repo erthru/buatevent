863db839b581b25b8e2ecda3cfd89220da9cb460
erthru <ersaka96@gmail.com>
2023-10-09 22:35:50 +0800

    Prepare events and category by slug

---
+++ pages/categories/[slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <p>kategori</p>
+</template>
+
+<script lang="ts" setup>
+const { public: prc } = useRuntimeConfig();
+
+useHead({
+  title: `.... | ${prc.appTitle}`,
+});
+</script>
---
+++ pages/events.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>kubun</p>
+</template>
+
+<script lang="ts" setup>
+const { public: prc } = useRuntimeConfig();
+
+useHead({
+  title: `Event | ${prc.appTitle}`,
+});
+</script>
